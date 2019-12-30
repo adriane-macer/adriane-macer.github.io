aec932677b47e60a6e9a5a4e5b0331b7d9f9d265
Adrian Evangelista <adriane.macer@gmail.com>
2019-12-30 21:44:18 +0800

    Removed non related website files

--- lib/core/shared/custom_enum.dart
+++
@@ -1,5 +0,0 @@
-enum ConnectivityStatus { WiFi, Cellular, Offline }
-
-enum PageLoadingStatus { Loading, Loaded, Error }
-
-enum ConfirmAction { CANCEL, ACCEPT }
--- lib/core/utils/array_object_helper.dart
+++
@@ -1,6 +0,0 @@
-class ArrayObjectHelper {
-  static String getFirstIndexArrayObject(Map<String, dynamic> json) {
-    var _list = json.keys.toList();
-    return json[_list[0]][0];
-  }
-}
