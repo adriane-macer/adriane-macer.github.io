26b1b3d82c10c626b67bc1a2ae68a1e36e2cf983
Adrian Evangelista <adriane.macer@gmail.com>
2019-12-30 20:36:04 +0800

    Updated pubspec packages

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Evangelista\Documents\GitHub\FH2019\fh2019_all_platforms\flutter.FH2019"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Evangelista\Documents\GitHub\FH2019\fh2019_web\adriane-macer.github.io"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
