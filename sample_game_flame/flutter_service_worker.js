c244a304df09ba8687cc4fa7f50797cffa9ca9c9
Adrian Evangelista <adriane.macer@gmail.com>
2023-11-07 23:12:11 +0800

    upadated sample game flame

--- sample_game_flame/flutter_service_worker.js
+++ sample_game_flame/flutter_service_worker.js
@@ -88,8 +88,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "96845130417b3f5067ca961fa852b931
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "f8f025bf06debf1ee47b36d04793df46",
-"/": "f8f025bf06debf1ee47b36d04793df46",
+"index.html": "1a88e4615b745922bc2963838c7e2131",
+"/": "1a88e4615b745922bc2963838c7e2131",
 "main.dart.js": "a97c398cac6ae916e1a34c1b307702ab",
 "manifest.json": "fde006e73dd49a3932045786d599a7a0",
 "version.json": "3d7fe6cb7fcc4de7e493d0bd8314edf8"};
